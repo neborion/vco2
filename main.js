3ddaaf591ba798b9d6d1778ed54bc860ad82d869
neborion <neb.services@protonmail.com>
2020-04-01 23:28:19 +0200

    Formatage durée & minor bug
    Formatage de la durée en unités décomposées. Commentaire code.
    Nettoyage code. Ajout ligne « À quelles conditions… ». Ajout mention
    cookies, privacy, RGPD. Réduction padding top&down des sections.
    Systématisation de l’ajustement bas de page anti-recouvrement à chaque
    calcul.

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
 /* Le code CSS ET JS de l'accordéon est dans un fichier séparé API-accordeon-deroulant.js/css */
 
-/* CALCUL */
+/* Fonctions de formatage - résultat en unité TEMPS */
+
+// Fonction de cosmetique orthographique
+function pluriel (inStr, nb, app){
+	if(nb>1){var plur="s";}else{var plur="";}
+	return inStr+" "+nb+" "+app+plur;
+}
 
 // fonction de formatage des durées. minutes -> mois/jours/heures/minutes.
 function formatDuration (x){
-	if(x>43200){var mois = parseInt(x/43200); x=x%43200;}
-	if(x>1440){var jours = parseInt(x/1440); x=x%1440;}
-	if(x>60){var heures = parseInt(x/60); var min=x%60;}else{var min=Math.round(x);}
-	return mois+" mois, "+jours+" jours, "+heures+" heures, "+min+" min. "+x;
+	if(x<1){return "moins d'une minute";}
+	var str = "";
+	if(x>43200){var mois = parseInt(x/43200); x=x%43200; str=str+mois+" mois";}
+	if(x>1440){var jours = parseInt(x/1440); x=x%1440; str=pluriel(str,jours,"jour");}
+	if(x>60){var heures = parseInt(x/60); var min=Math.round(x%60); str=pluriel(str,heures,"heure");}else{var min=Math.round(x);}
+	str=pluriel(str,min,"minute");
+	return str;
 }
+
+/* AJUSTEMENT DE LA MARGE DE FIN DE SCROLL POUR ÉVITER RECOUVREMENT */
+function ajustBottomVoidSpace(){
+	var result_height = document.getElementById('section_result').clientHeight;
+	document.getElementById("main").style.margin = "0px 0px "+String(result_height+10)+"px 0px";
+}
+
+/* CALCUL */
 
 function calcul (){
 	// Les valeurs du calcul sont données par l'attribut "value" des balises input du formulaire d'index.html
-
-
 
 	// On regarde quelle box est checké (qualité vidéo) :
 	var qualiteElementsList = document.getElementsByName("qualite_choice");
@@ -44,14 +59,17 @@ function calcul (){
 			var intensite_carbone_mix_elec_france = parseFloat(mixElementList[i].value);
 		}
 	}
+	// On regarde la durée de vidéo renseigné :
 	var duree = parseInt(document.getElementById("time").value);
+
+	// CALCULS : (Avec étapes)
 	var poid_total = duree * poidParMinute;
 	var energie_device = energie_flux_device * duree;
-	var energie_serveur = poid_total * 0.072;
-	// Poid(en Mo) * conso Data-Center (=7.2E-11 kWh/Octet = 0,072Wh/Mo)
-
+	var energie_serveur = poid_total * 0.072; // Poid(en Mo) * conso Data-Center (=7.2E-11 kWh/Octet = 0,072Wh/Mo)
 	var energie_infrastructure = energie_flux_infrastructure * poid_total;
 	var energie_totale = energie_device + energie_serveur + energie_infrastructure;
+
+	// RÉSULTATS :
 	var result = (energie_device + energie_serveur + energie_infrastructure) * (intensite_carbone_mix_elec_france/1000);
 	// On divise l'intensité carbone par 1000 car elle est donnée en g/kWh. On la veut en g/Wh.
 	var resultTemps = result * (365*24*60) / 2000000;
@@ -69,16 +87,18 @@ function calcul (){
 	document.getElementById("consoTotale").innerHTML = energie_totale.toFixed(2);
 	var span_result_to_update = document.getElementsByClassName("result");
 	for (var i = span_result_to_update.length - 1; i >= 0; i--){span_result_to_update[i].innerHTML = result.toFixed(2);}
-	document.getElementById("equitemps").innerHTML = Math.round(resultTemps);
-
-	console.log(formatDuration(resultTemps));
+	document.getElementById("equitemps").innerHTML = formatDuration(resultTemps);
+	
+	ajustBottomVoidSpace() // Ajustement mis-en-page anti-recouvrement bas de page.
 }
+
+/* EXECUTION AU CHARGEMENT - INIT */
 
 window.onload = function(){
 
 	chopperEtActiverAccordeon();//Code de la fonction dans API-accordeon-deroulant.js
 
-	// Appliquer les régalges par défaut aux checkbox-ratio
+	// Appliquer les réglages par défaut aux checkbox-ratio (quelles paramètres réglés par défaut au démarrage);
 	var ratioCheckedByDefaultList = document.getElementsByClassName("default");
 	for (var i = ratioCheckedByDefaultList.length - 1; i >= 0; i--) {
 		ratioCheckedByDefaultList[i].checked = true;
@@ -90,11 +110,6 @@ window.onload = function(){
 		listenerList[i].addEventListener("click", calcul, false);
 	}
 
-	// Lancement du calcul dès chargement de la page
-	formatDuration(calcul());
-
-	/* AJUSTEMENT DE LA MARGE DE FIN DE SCROLL POUR ÉVITER RECOUVREMENT */
-	var result_height = document.getElementById('section_result').clientHeight;
-	document.getElementById("main").style.margin = "0px 0px "+String(result_height)+"px 0px";
-	
+	// Lancement du premier calcul au chargement de la page
+	calcul();
 }
